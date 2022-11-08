07558afeef7e9c73332db1ba0a946fba20eab7b9
FajarAlRahman <al24fajar@gmail.com>
2022-11-08 11:13:07 +0700

    First Commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Dosen dosen1 = new Dosen(123, "Erson Bridges", "Kota Gede, Yogyakarta", 12,12,1999, "Laki-Laki", 2012, 2012001, "Informatika",8 );
+        dosen1.cetak();
+
+        Tendik tendik1 = new Tendik(222,"Ahmad", "Bantul, Yogyakarta",24,01,1975,"Laki-Laki", 2009, 15);
+        tendik1.cetak();
+    }
+}
